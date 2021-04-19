e4822f41a8f20efb1b8c0bf3f68fc5d0d8883ad3
Salvatore <backsofangels@gmail.com>
2021-04-19 23:32:42 +0200

    Change return type of deletion

--- reservation.h
+++ reservation.h
@@ -16,7 +16,7 @@ Reservation *createReservation(char *customerFiscalCode, int timeOfTheDay);
 Reservation *performReservation(Reservation *r, char *customerFiscalCode);
 Reservation *insertReservationOnEnd(Reservation *head, Reservation *r);
 Reservation *mergeReservationLists(Reservation *symptomatics, Reservation *asymptomatics);
-void deleteReservation(Reservation *r, int reservationId);
+Reservation *deleteReservation(Reservation *r, int reservationId);
 void saveReservationOnFile(Reservation *headOfReservation, char *filename, char *mode);
 void printReservations(Reservation *r, FILE *file, int printOnFile);
 Reservation *searchReservationById(Reservation *head, int id);
