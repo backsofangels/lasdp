dab7769228262a3b07e3cd79a75a82bd2e18f6bf
Salvatore <backsofangels@gmail.com>
2021-04-24 11:28:19 +0200

    Fix functions naming and add print by fc

--- appointments.h
+++ appointments.h
@@ -7,12 +7,13 @@ typedef struct Day {
     PtrReservation timeOfDay[3];
 } Day;
 
-Day initializeNewDay();
+Day initializeDay();
 Day disponiAppuntamentiNew();
 int countElementsInQueue(Reservation *resLis);
 void printDailyAppointmentsOnFile(Day day, int timeOfDay, FILE *file, int printOnFile);
 void printDailyAppointmentsWrapper(int printOnFile, Day day);
 Day removeAppointmentById(Day day, int id);
 Day addAppointmentById();
+void printAppointmentByFiscalCode(Day day, char *fiscalCode);
 
 #endif
