b2bef652c511fe1fdc66b5232f037bd0e414dff7
Salvatore <backsofangels@gmail.com>
2021-04-26 21:27:17 +0200

    Fix for clarity to reservations prompts

--- reservation.c
+++ reservation.c
@@ -20,6 +20,8 @@ Reservation *performReservation(Reservation *headOfReservation, char *fiscalCode
     printf("Quando vorresti fare il tampone?\n1. Mattina\n2. Pomeriggio\n3. Sera\n");
     scanf("%d", &timeOfTheDay);
     Reservation *r = createReservation(fiscalCode, timeOfTheDay);
+    printf("\nHai creato la prenotazione, questi sono i dati\n");
+    printSingleReservation(r);
     return insertReservationOnEnd(headOfReservation, r);
 }
 
@@ -32,19 +34,34 @@ Reservation *insertReservationOnEnd(Reservation *head, Reservation *r) {
     return head;
 }
 
-Reservation *deleteReservation(Reservation *r, int reservationId) {
-    if (r == NULL) { return r; }
+Reservation *deleteReservation(Reservation *r, int reservationId, int *hasCancelled) {
+    if (r == NULL) {
+        return r;
+    }
     if (r->reservationId == reservationId) {
         Reservation *tmp = r->nextReservation;
         r->nextReservation = NULL;
         free(r);
+        *hasCancelled = 1;
         return tmp;
-    } else r->nextReservation = deleteReservation(r->nextReservation, reservationId);
+    } else r->nextReservation = deleteReservation(r->nextReservation, reservationId, hasCancelled);
     return r;
 }
 
+int checkReservationAlreadyPerformed(char *fiscalCode) {
+    Reservation *syntomatic = NULL;
+    Reservation *asyntomatic = NULL;
+
+    syntomatic = loadReservationsFromFile("symptomatics.txt");
+    asyntomatic = loadReservationsFromFile("asymptomatics.txt");
+
+    return searchReservationByFiscalCode(syntomatic, fiscalCode) || searchReservationByFiscalCode(asyntomatic, fiscalCode);
+}
 
 Reservation *mergeReservationLists(Reservation *symptomatics, Reservation *asymptomatics) {
+    if (symptomatics == NULL) {
+        return asymptomatics;
+    }
     Reservation *mergedList = symptomatics;
     while (symptomatics->nextReservation != NULL) {
         symptomatics = symptomatics->nextReservation;
@@ -58,32 +75,12 @@ void saveReservationOnFile(Reservation *headOfReservation, char *filename, char 
     file = fopen(filename, mode);
 
     if (file == NULL) {
-        printf("nullFile\n");
         printReservations(headOfReservation, NULL, 0);
     } else if (file != NULL) {
         printReservations(headOfReservation, file, 1);
     }
 
     fclose(file);
-}
-
-Reservation *searchReservationById(Reservation *headOfReservation, int reservationId) {
-    if (headOfReservation == NULL) {
-        return headOfReservation;
-    } else if (headOfReservation->reservationId == reservationId) {
-        return headOfReservation;
-    }
-    headOfReservation = searchReservationById(headOfReservation->nextReservation, reservationId);
-}
-
-Reservation *searchReservationByCustomer(Reservation *headOfReservation, char *customerFiscalCode) {
-    if (headOfReservation == NULL) {
-        return headOfReservation;
-    }
-    if (strcmp(headOfReservation->fiscalCodeCustomer, customerFiscalCode) == 0) {
-        return headOfReservation;
-    }
-    headOfReservation = searchReservationByCustomer(headOfReservation->nextReservation, customerFiscalCode);
 }
 
 Reservation *searchReservationByTimeOfDay (Reservation *headOfReservation, int timeOfTheDay) {
@@ -102,16 +99,59 @@ Reservation *searchReservationByTimeOfDay (Reservation *headOfReservation, int t
     return results;
 }
 
+int searchReservationByFiscalCode(PtrReservation head, char *fiscalCode) {
+    if (head == NULL) {
+        return 0;
+    }
+    if (strcasecmp(head->fiscalCodeCustomer, fiscalCode) == 0 ) {
+        return 1;
+    } else {
+        return searchReservationByFiscalCode(head->nextReservation, fiscalCode);
+    }
+}
+
 void printReservations(Reservation *head, FILE *file, int printOnFile) {
     if (head == NULL) {
         return;
     }
     if (file != NULL && printOnFile == 1) {
         fprintf(file, "%d\t%s\t%d\n", head->reservationId, head->fiscalCodeCustomer, head->timeOfTheDay);
-    } else if (printOnFile == 0) { 
-        printf("%d\t%s\t%d\n", head->reservationId, head->fiscalCodeCustomer, head->timeOfTheDay); 
+    } else if (printOnFile == 0) {
+        printSingleReservation(head);
     }
     printReservations(head->nextReservation, file, printOnFile);
+}
+
+void printReservationByName(PtrReservation head, char *fiscalCode) {
+    if (head == NULL) {
+        return;
+    }
+    if (head != NULL) {
+        if (strcmp(head->fiscalCodeCustomer, fiscalCode) == 0) {
+            printSingleReservation(head);
+        }
+    }
+    printReservationByName(head->nextReservation, fiscalCode);
+}
+
+void printSingleReservation(PtrReservation head) {
+    if (head != NULL) {
+        printf("\tIdentificativo della prenotazione: %d\n", head->reservationId);
+        printf("\tCodice fiscale del paziente: %s\n", head->fiscalCodeCustomer);
+        switch (head->timeOfTheDay) {
+        case 1:
+            printf("\tMomento dell'appuntamento: mattina\n");
+            break;
+        case 2:
+            printf("\tMomento dell'appuntamento: pomeriggio\n");
+            break;
+        case 3:
+            printf("\tMomento dell'appuntamento: sera\n");
+            break;
+        default:
+            break;
+        }
+    }
 }
 
 Reservation *loadReservationsFromFile(char *filename) {
@@ -124,6 +164,12 @@ Reservation *loadReservationsFromFile(char *filename) {
     if (file == NULL) {
         return NULL;
     }
+
+    if (fgetc(file) == EOF) {
+        return NULL;
+    }
+
+    fseek(file, 0, SEEK_SET);
 
     do {
         int reservationId;
@@ -145,8 +191,6 @@ void printMergedListsOnFileWrapper() {
     Reservation *asymptReservations = NULL;
     symptReservations = loadReservationsFromFile("symptomatics.txt");
     asymptReservations = loadReservationsFromFile("asymptomatics.txt");
-
     Reservation *merged = mergeReservationLists(symptReservations, asymptReservations);
-
     saveReservationOnFile(merged, "reservations.txt", "w");
 }
--- reservation.h
+++ reservation.h
@@ -16,12 +16,14 @@ Reservation *createReservation(char *customerFiscalCode, int timeOfTheDay);
 Reservation *performReservation(Reservation *r, char *customerFiscalCode);
 Reservation *insertReservationOnEnd(Reservation *head, Reservation *r);
 Reservation *mergeReservationLists(Reservation *symptomatics, Reservation *asymptomatics);
-Reservation *deleteReservation(Reservation *r, int reservationId);
+Reservation *deleteReservation(Reservation *r, int reservationId, int *hasCancelled);
+int checkReservationAlreadyPerformed(char *fiscalCode);
 void saveReservationOnFile(Reservation *headOfReservation, char *filename, char *mode);
 void printReservations(Reservation *r, FILE *file, int printOnFile);
-Reservation *searchReservationById(Reservation *head, int id);
-Reservation *searchReservationByCustomer(Reservation *head, char *fiscalCode);
+void printReservationByName(PtrReservation head, char *fiscalCode);
+void printSingleReservation(PtrReservation head);
 Reservation *searchReservationByTimeOfDay(Reservation *head, int timeOfTheDay);
+int searchReservationByFiscalCode(PtrReservation head, char *fiscalCode);
 Reservation *loadReservationsFromFile(char *filename);
 void printMergedListsOnFileWrapper();
 #endif
