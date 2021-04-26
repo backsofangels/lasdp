731c71cf0fd2f3d24f91502ab9074c6a8ac8c8d2
Salvatore <backsofangels@gmail.com>
2021-04-26 21:28:13 +0200

    Add functions to check file existences

--- utils.c
+++ utils.c
@@ -24,3 +24,108 @@ void emptyAppointmentsFile() {
 
     fclose(appointments);
 }
+
+void initDatabase() {
+    FILE *genericDbTable = NULL;
+
+    genericDbTable = fopen("reservations.txt", "r");
+
+    if (genericDbTable == NULL) {
+        genericDbTable = fopen("reservations.txt", "w");
+        fclose(genericDbTable);
+    }
+
+    genericDbTable = fopen("daily-appointments.txt", "r");
+
+    if (genericDbTable == NULL) {
+        genericDbTable = fopen("daily-appointments.txt", "w");
+        fclose(genericDbTable);
+    }
+
+    genericDbTable = fopen("covid-test-history.txt", "r");
+
+    if (genericDbTable == NULL) {
+        genericDbTable = fopen("covid-test-history.txt", "w");
+        fclose(genericDbTable);
+    }
+
+    genericDbTable = fopen("symptomatics.txt", "r");
+
+    if (genericDbTable == NULL) {
+        genericDbTable = fopen("symptomatics.txt", "w");
+        fclose(genericDbTable);
+    }
+
+    genericDbTable = fopen("symptomatics.txt", "r");
+
+    if (genericDbTable == NULL) {
+        genericDbTable = fopen("symptomatics.txt", "w");
+        fclose(genericDbTable);
+    }
+
+    genericDbTable = fopen("asymptomatics.txt", "r");
+
+    if (genericDbTable == NULL) {
+        genericDbTable = fopen("asymptomatics.txt", "w");
+        fclose(genericDbTable);
+    }
+
+    genericDbTable = fopen("quarantine-log.txt", "r");
+
+    if (genericDbTable == NULL) {
+        genericDbTable = fopen("quarantine-log.txt", "w");
+        fclose(genericDbTable);
+    }
+
+    genericDbTable = fopen("customerRegistration.txt", "r");
+
+    if (genericDbTable == NULL) {
+        genericDbTable = fopen("customerRegistration.txt", "w");
+        fclose(genericDbTable);
+    }
+
+    genericDbTable = fopen("testcenterregistration.txt", "r");
+
+    if (genericDbTable == NULL) {
+        genericDbTable = fopen("testcenterregistration.txt", "w");
+        fclose(genericDbTable);
+    }
+}
+
+void clearScreen() {
+    #ifdef _WIN32
+    system("cls");
+    #endif // _WIN32
+
+    #ifdef __linux__
+    system("clear");
+    #endif // __linux__
+}
+
+void waitInputPrint(char *message) {
+    printf("%s\n", message);
+    fflush(stdin);
+    getchar();
+    fflush(stdin);
+}
+
+int sanitizeUserInput(int maxUserInput) {
+    int temporaryInput, userchoice = -1;
+
+    fflush(stdin);
+    if (scanf("%d", &temporaryInput) == 1) {
+        fflush(stdin);
+        if (temporaryInput > 0 && temporaryInput < maxUserInput) {
+            userchoice = temporaryInput;
+        } else {
+            printf("Scelta non corretta, riprovare\n");
+            waitInputPrint("Premi INVIO per tornare al menu'");
+        }
+    } else {
+        fflush(stdin);
+        printf("Scelta non corretta, riprovare\n");
+        waitInputPrint("Premi INVIO per tornare");
+    }
+    return userchoice;
+}
+
--- utils.h
+++ utils.h
@@ -4,5 +4,9 @@
 void emptyReservationListsFiles();
 void emptyCommonReservationList();
 void emptyAppointmentsFile();
+void initDatabase();
+void clearScreen();
+void waitInputPrint(char *message);
+int sanitizeUserInput(int maxUserInput);
 
 #endif
